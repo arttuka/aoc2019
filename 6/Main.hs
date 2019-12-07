19c0aaf20aaf33387a74508d1be3952406fac332
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2019-12-07 22:19:10 +0200

    Day 6 part 2

--- 6/Main.hs
+++ 6/Main.hs
@@ -4,22 +4,25 @@ import Prelude hiding (lookup)
 import Control.Applicative
 import Control.Monad
 import Data.List.Split
-import Data.Map.Strict
+import Data.Map.Strict hiding (map, drop)
 import Data.Tree
 
 type Orbit = (String, String)
 type OrbitTree = Tree String
 
-findOrbitRoot :: [Orbit] -> String
-findOrbitRoot orbits =
-  let childToParent = fromList $ Prelude.map (\(parent, child) -> (child, parent)) orbits
-      findRoot x = maybe x findRoot (lookup x childToParent)
+longestCommonPrefix :: Eq a => [a] -> [a] -> [a]
+longestCommonPrefix [] _ = []
+longestCommonPrefix _ [] = []
+longestCommonPrefix (x:xs) (y:ys) = if x == y then x : longestCommonPrefix xs ys else []
+
+findOrbitRoot :: [Orbit] -> Map String String -> String
+findOrbitRoot orbits childToParent =
+  let findRoot x = maybe x findRoot (lookup x childToParent)
   in findRoot (fst $ head orbits)
 
-buildOrbitTree :: [Orbit] -> OrbitTree
-buildOrbitTree orbits =
-  let parentToChildren = fromListWith (++) $ Prelude.map (\(parent, child) -> (parent, [child])) orbits
-      root = findOrbitRoot orbits
+buildOrbitTree :: String -> [Orbit] -> OrbitTree
+buildOrbitTree root orbits =
+  let parentToChildren = fromListWith (++) $ map (\(parent, child) -> (parent, [child])) orbits
       buildNode x = (x, findWithDefault [] x parentToChildren)
   in unfoldTree buildNode root
 
@@ -28,6 +31,27 @@ countOrbits orbitTree =
   let sumLevels _ [] = 0
       sumLevels n (lvl:lvls) = (n * length lvl) + sumLevels (n + 1) lvls
   in sumLevels 0 $ levels orbitTree
+
+findRouteFromRoot :: String -> OrbitTree -> Maybe [String]
+findRouteFromRoot end (Node label children)
+  | end == label = Just [end]
+  | otherwise    = fmap ((:) label) $ msum $ fmap (findRouteFromRoot end) children
+
+findRoute :: String -> String -> OrbitTree -> Maybe [String]
+findRoute start end tree = do
+  routeToStart <- findRouteFromRoot start tree
+  routeToEnd   <- findRouteFromRoot end tree
+  let prefix = longestCommonPrefix routeToStart routeToEnd
+      len    = length prefix
+      startPath = drop (len - 1) routeToStart
+      endPath   = drop len routeToEnd
+  Just $ reverse startPath ++ endPath
+
+findRouteFromYouToSan :: Map String String -> OrbitTree -> Maybe [String]
+findRouteFromYouToSan childToParent tree = do
+  start <- lookup "YOU" childToParent
+  end <- lookup "SAN" childToParent
+  findRoute start end tree
 
 readOrbit :: String -> Orbit
 readOrbit s = (a, b)
@@ -38,5 +62,8 @@ readOrbits s = readOrbit <$> lines s
 
 main :: IO ()
 main = do orbits <- fmap readOrbits getContents
-          let orbitTree = buildOrbitTree orbits
-          print $ countOrbits orbitTree
+          let childToParent = fromList $ map (\(parent, child) -> (child, parent)) orbits
+              root = findOrbitRoot orbits childToParent
+              orbitTree = buildOrbitTree root orbits
+              route = findRouteFromYouToSan childToParent orbitTree
+          print $ length <$> route
