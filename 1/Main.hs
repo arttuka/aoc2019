3a86f7d832d3e970dc0cae2520a9e76607d206f3
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2019-12-07 11:50:50 +0200

    Day 1 part 2

--- 1/Main.hs
+++ 1/Main.hs
@@ -7,7 +7,10 @@ toInt :: String -> Integer
 toInt s = read s :: Integer
 
 massToFuel :: Integer -> Integer
-massToFuel mass = (mass `div` 3) - 2
+massToFuel mass
+  | mass < 9  = 0
+  | otherwise = fuel + massToFuel fuel
+  where fuel = (mass `div` 3) - 2
 
 calculateTotalFuel :: [String] -> Integer
 calculateTotalFuel strMasses = sum masses
