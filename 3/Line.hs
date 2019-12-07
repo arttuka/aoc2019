318b0b7776ae140fb8291b78d1f3bcf06d20dd11
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2019-12-07 16:07:14 +0200

    Day 3 part 2

--- 3/Line.hs
+++ 3/Line.hs
@@ -1,30 +1,58 @@
 module Line where
 
-import Data.Maybe
+import Control.Applicative
 
-data Line = VLine Int Int Int | HLine Int Int Int deriving (Show)
-data Intersection = Intersection Int Int Int deriving (Show)
+type Intersection = (Int, Int)
+data Line = VLine Int Int Int [Intersection] | HLine Int Int Int [Intersection]
+
+instance Show Line where
+  show (VLine x y d ints) = "VLine (" ++ show x ++ "," ++ show y ++ ") (" ++ show x ++ "," ++ show (y + d) ++ ") " ++ show ints
+  show (HLine x y d ints) = "HLine (" ++ show x ++ "," ++ show y ++ ") (" ++ show (x + d) ++ "," ++ show y ++ ") " ++ show ints
+
+intersections :: Line -> [Intersection]
+intersections (VLine _ _ _ ints) = ints
+intersections (HLine _ _ _ ints) = ints
+
+lineLength :: Line -> Int
+lineLength (VLine _ _ d _) = abs d
+lineLength (HLine _ _ d _) = abs d
 
 between :: Int -> Int -> Int -> Bool
-between a b x = a <= x && x <= b
+between a b x = (a <= x && x <= b) || (b <= x && x <= a)
 
-intersection :: Line -> Line -> Maybe Intersection
-intersection (VLine x0 y0 d0) (HLine x1 y1 d1) =
-  if between y0 (y0 + d0) y1 && between x1 (x1 + d1) x0
-    then Just $ Intersection x0 y1 (abs x0 + abs y1)
-    else Nothing
-intersection l1@HLine {} l2@VLine {} = intersection l2 l1
-intersection VLine {} VLine {} = Nothing
-intersection HLine {} HLine {} = Nothing
+intersect :: Line -> Line -> (Line, Line)
+intersect l1@(VLine x0 y0 d0 i0) l2@(HLine x1 y1 d1 i1)
+  | int == (0, 0) = (l1, l2)
+  | between y0 (y0 + d0) y1 && between x1 (x1 + d1) x0 = (VLine x0 y0 d0 (int : i0), HLine x1 y1 d1 (int : i1))
+  | otherwise = (l1, l2)
+  where int = (x0, y1)
+intersect l1@HLine {} l2@VLine {} = (line1, line2)
+  where (line2, line1) = l2 `intersect` l1
+intersect l1 l2 = (l1, l2)
 
-isNotOrigin :: Intersection -> Bool
-isNotOrigin (Intersection _ _ d) = d /= 0
+intersectOneFolder :: Line -> (Line, [Line]) -> (Line, [Line])
+intersectOneFolder nextLine (currentLine, previousLines) = (newLine1, newLine2 : previousLines)
+  where
+    (newLine1, newLine2) = currentLine `intersect` nextLine
 
-findIntersections :: [Line] -> [Line] -> [Intersection]
-findIntersections lines1 lines2 = filter isNotOrigin intersections
-  where intersections = catMaybes [intersection l1 l2 | l1 <- lines1, l2 <- lines2]
+intersectOne :: Line -> [Line] -> (Line, [Line])
+intersectOne line = Prelude.foldr intersectOneFolder (line, [])
 
-closerIntersection :: Intersection -> Intersection -> Intersection
-closerIntersection i1@(Intersection _ _ d0) i2@(Intersection _ _ d1)
-  | d0 < d1   = i1
-  | otherwise = i2
+intersectAllFolder :: Line -> ([Line], [Line]) -> ([Line], [Line])
+intersectAllFolder line (firstLines, secondLines) = (newLine : firstLines, newSecondLines)
+  where
+    (newLine, newSecondLines) = line `intersectOne` secondLines
+
+intersectAll :: [Line] -> [Line] -> ([Line], [Line])
+intersectAll lines1 lines2 = Prelude.foldr intersectAllFolder ([], lines2) lines1
+
+calculateDistanceForIntersection :: Int -> Line -> Intersection -> (Intersection, Int)
+calculateDistanceForIntersection distance (VLine _ y _ _) int@(_, y0) = (int, distance + abs (y - y0))
+calculateDistanceForIntersection distance (HLine x _ _ _) int@(x0, _) = (int, distance + abs (x - x0))
+
+calculateDistancesForLine :: Int -> Line -> [(Intersection, Int)]
+calculateDistancesForLine distance line = calculateDistanceForIntersection distance line <$> intersections line
+
+calculateDistances :: Int -> [Line] -> [(Intersection, Int)]
+calculateDistances distance [] = []
+calculateDistances distance (line:lines) = calculateDistancesForLine distance line ++ calculateDistances (distance + lineLength line) lines 
--- 3/Main.hs
+++ 3/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Control.Applicative
 import Control.Monad
 import Data.List.Split
+import Data.Map.Strict
 import Line
 
 data PathSegment = PathUp Int | PathDown Int | PathLeft Int | PathRight Int deriving (Show)
@@ -15,10 +16,10 @@ pathToLines _ _ [] = []
 pathToLines x y (segment:segments) = line : pathToLines x1 y1 segments
   where
     (x1, y1, line) = case segment of
-      PathUp d    -> (x, y - d, VLine x (y - d) d)
-      PathDown d  -> (x, y + d, VLine x y d)
-      PathLeft d  -> (x - d, y, HLine (x - d) y d)
-      PathRight d -> (x + d, y, HLine x y d)
+      PathUp d    -> (x, y - d, VLine x y (-d) [])
+      PathDown d  -> (x, y + d, VLine x y d [])
+      PathLeft d  -> (x - d, y, HLine x y (-d) [])
+      PathRight d -> (x + d, y, HLine x y d [])
 
 readLines :: IO [String]
 readLines = fmap lines getContents
@@ -34,12 +35,16 @@ readPathSegment (x:xs) = case x of
 readPath :: String -> [PathSegment]
 readPath s = readPathSegment <$> splitOn "," s
 
-closestIntersection :: [Intersection] -> Intersection
-closestIntersection = foldl1 closerIntersection
+closerIntersection :: Maybe (Intersection, Int) -> Intersection -> Int -> Maybe (Intersection, Int)
+closerIntersection z i2 d2 = case z of
+  Just (i1, d1) -> if d1 < d2 then z else Just (i2, d2)
+  Nothing -> Just (i2, d2)
 
 main :: IO ()
 main = do ls <- readLines
           let paths = fmap readPath ls
           let [lines1, lines2] = fmap (pathToLines 0 0) paths
-          let intersections = findIntersections lines1 lines2
-          print $ closestIntersection intersections
+          let (intersected1, intersected2) = intersectAll lines1 lines2
+          let distances = fromListWith (+) (calculateDistances 0 intersected1 ++ calculateDistances 0 intersected2)
+          let closestIntersection = foldlWithKey closerIntersection Nothing distances
+          print closestIntersection
