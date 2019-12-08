9f7f42b0df449a5afc1ce0c4aaeea07b6c5706ee
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2019-12-08 13:31:29 +0200

    Day 8 part 2

--- 8/Main.hs
+++ 8/Main.hs
@@ -5,9 +5,9 @@ import Control.Monad
 import Data.Char
 import Data.List
 import Data.List.Split
-import Data.Ord
 
-pixelsPerLayer = 25 * 6
+width = 25
+height = 6
 
 numberOf :: Int -> [Int] -> Int
 numberOf i layer = length $ filter (==i) layer
@@ -15,15 +15,29 @@ numberOf i layer = length $ filter (==i) layer
 valueOfLayer :: [Int] -> Int
 valueOfLayer layer = numberOf 1 layer * numberOf 2 layer
 
-splitToLayers :: [Int] -> [[Int]]
-splitToLayers = chunksOf pixelsPerLayer
+showPixel :: Int -> String
+showPixel 0 = "#"
+showPixel _ = " "
 
-splitToDigits :: String -> [Int]
-splitToDigits = map digitToInt 
+showLine :: [Int] -> String
+showLine l = intercalate "" $ map showPixel l
+
+showImage :: [Int] -> String
+showImage image = intercalate "\n" $ map showLine lines
+  where lines = chunksOf width image
+
+stackPixels :: Int -> Int -> Int
+stackPixels 0 _ = 0
+stackPixels 1 _ = 1
+stackPixels 2 x = x
+
+stackLayers :: [[Int]] -> [Int]
+stackLayers = foldl1 (zipWith stackPixels)
+
+splitToLayers :: String -> [[Int]]
+splitToLayers s = chunksOf (width * height) $ map digitToInt s
 
 main :: IO ()
 main = do contents <- getContents
-          let digits = splitToDigits contents
-              layers = splitToLayers digits
-              minLayer = minimumBy (comparing $ numberOf 0) layers
-          print $ valueOfLayer minLayer
+          let image = showImage . stackLayers . splitToLayers $ contents
+          putStr image
