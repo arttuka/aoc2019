2adabe6f273aadb193a01052d43d2bb0fe797766
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2019-12-29 12:33:01 +0200

    Day 12 part 2

--- 12/Main.hs
+++ 12/Main.hs
@@ -7,6 +7,7 @@ import Text.Regex.PCRE ((=~), getAllTextSubmatches)
 
 type Coord = (Int, Int, Int)
 type Moon = (Coord, Coord)
+type Axis = (Int, Int)
 
 toInt :: String -> Int
 toInt s = read s :: Int
@@ -30,8 +31,8 @@ abssum (a, b, c) = abs a + abs b + abs c
 calculateEnergy :: Moon -> Int
 calculateEnergy (pos, v) = abssum pos * abssum v
 
-applyGravityOnAxis :: Int -> Int -> Int
-applyGravityOnAxis a b
+gravityOnAxis :: Int -> Int -> Int
+gravityOnAxis a b
   | a < b     = 1
   | a > b     = -1
   | otherwise = 0
@@ -39,9 +40,9 @@ applyGravityOnAxis a b
 applyGravity :: Moon -> Moon -> Moon
 applyGravity (pos@(x1, y1, z1), v) ((x2, y2, z2), _) = (pos, add3 v (dvx, dvy, dvz))
   where
-    dvx = applyGravityOnAxis x1 x2
-    dvy = applyGravityOnAxis y1 y2
-    dvz = applyGravityOnAxis z1 z2
+    dvx = gravityOnAxis x1 x2
+    dvy = gravityOnAxis y1 y2
+    dvz = gravityOnAxis z1 z2
 
 applyVelocity :: Moon -> Moon
 applyVelocity (pos, v) = (add3 pos v, v)
@@ -51,7 +52,30 @@ simulateStep moons = map applyVelocity gravityApplied
   where
     gravityApplied = map (\m -> foldl' applyGravity m moons) moons
 
+applyGravityOnAxis :: Axis -> Axis -> Axis
+applyGravityOnAxis (t1, v1) (t2, _) = (t1, v1 + gravityOnAxis t1 t2)
+
+applyVelocityOnAxis :: Axis -> Axis
+applyVelocityOnAxis (t, v) = (t + v, v)
+
+simulateStepOnAxis :: [Axis] -> [Axis]
+simulateStepOnAxis axes = map applyVelocityOnAxis gravityApplied
+  where
+    gravityApplied = map (\a -> foldl' applyGravityOnAxis a axes) axes
+
+moonToAxes :: Moon -> (Axis, Axis, Axis)
+moonToAxes ((x, y, z), (dx, dy, dz)) = ((x, dx), (y, dy), (z, dz))
+
+findCycle :: (Eq a) => (a -> a) -> a -> Int
+findCycle f x = step 1 x (f x)
+  where
+    step n t h
+        | t == h    = n
+        | otherwise = step (succ n) (f t) (f (f h))
+
 main :: IO ()
 main = do contents <- fmap lines getContents
           let moons = map parseRow contents
-          print $ sum $ map calculateEnergy $ applyN 1000 simulateStep moons
+              (xs, ys, zs) = unzip3 $ map moonToAxes moons
+              cycles = map (findCycle simulateStepOnAxis) [xs, ys, zs]
+          print $ foldl1 lcm cycles
