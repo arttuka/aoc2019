38e43eb2b77e16411d1280239974c844456338cf
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2020-12-28 11:20:34 +0200

    Day 22 part 2

--- 22/Main.hs
+++ 22/Main.hs
@@ -1,13 +1,16 @@
 module Main where
 
-import Control.Applicative
-import Control.Monad
-import Data.List (elemIndex, foldl', isPrefixOf, transpose)
-import Data.List.Split (chunksOf)
-import Util
+import Control.Monad (join)
+import Data.List (foldl', foldr, isPrefixOf, scanl', sortOn)
+import Util (absMod, combineTimes, dec2bin, mmi)
 
-data Technique = NewStack | Cut Int | Deal Int deriving Show
-type Deck = [Int]
+data Technique = NewStack | Cut Integer | Deal Integer deriving Show
+
+deckSize = 119315717514047
+rounds   = 101741582076661
+
+m :: Integer -> Integer
+m = (`absMod` deckSize)
 
 readLine :: String -> Technique
 readLine "deal into new stack"             = NewStack
@@ -15,18 +18,30 @@ readLine s
     | "deal with increment" `isPrefixOf` s = Deal $ read (drop 20 s)
     | "cut" `isPrefixOf` s                 = Cut $ read (drop 4 s)
 
-applyTechnique :: Int -> Deck -> Technique -> Deck
-applyTechnique deckSize deck technique = case technique of
-    NewStack -> reverse deck
-    Cut i    -> rotate (i `mod` deckSize) deck
-    Deal i   -> let factor        = getFactor i deckSize
-                    transformDeck = (join <$> transpose) . chunksOf factor
-                in take deckSize $ transformDeck $ take (i * factor) $ cycle deck
+applyTechnique :: Technique -> (Integer, Integer)
+applyTechnique NewStack = (-1, deckSize - 1)
+applyTechnique (Cut n)  = (1, -n)
+applyTechnique (Deal n) = (n, 0)
+
+reverseApplyTechnique :: Technique -> (Integer, Integer)
+reverseApplyTechnique NewStack = (-1, deckSize - 1)
+reverseApplyTechnique (Cut n)  = (1, n)
+reverseApplyTechnique (Deal n) = (mmi n deckSize, 0)
+
+combineTransforms :: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer)
+combineTransforms (a1, b1) (a2, b2) = (m (a1 * a2), m (b1 * a2 + b2))
+
+applyTransform :: (Integer, Integer) -> Integer -> Integer
+applyTransform (a, b) x = m (a * x + b)
+
+makeTransform :: [Technique] -> (Integer, Integer)
+makeTransform = foldl' combineTransforms (1, 0) . map applyTechnique
+
+makeReverseTransform :: [Technique] -> (Integer, Integer)
+makeReverseTransform = foldl' combineTransforms (1, 0) . reverse . map reverseApplyTechnique
 
 main :: IO ()
 main = do ls <- lines <$> getContents
           let techniques = map readLine ls
-              deckSize   = 10007
-              deck       = [0..(pred deckSize)]
-              result     = foldl' (applyTechnique deckSize) deck techniques
-          print $ elemIndex 2019 result
+              tf = combineTimes rounds combineTransforms $ makeReverseTransform techniques
+          print $ applyTransform tf 2020
--- 22/Util.hs
+++ 22/Util.hs
@@ -1,28 +1,43 @@
 module Util where
 
-import Control.Monad
-import Data.Tuple (swap)
+import Debug.Trace (trace)
 
-rotate :: Int -> [a] -> [a]
-rotate n xs = zipWith const (drop n (cycle xs)) xs
+import Control.Monad (join)
+import Data.List (foldl1)
+import Data.Tuple.Select (sel1)
 
-range :: Int -> Int -> Int -> [Int]
-range start end step = [start, start + step .. end]
+type Euclid = (Integer, Integer, Integer)
 
-getFactor :: Int -> Int -> Int
-getFactor a b = s `mod` b
+euclid :: Integer -> Integer -> Euclid
+euclid a b = last $ takeWhile ((0 /=) . sel1) results
   where
-    (_, _, s, _) = euclid a b
-
-euclid :: Int -> Int -> (Int, Int, Int, Int)
-euclid a b = last $ takeWhile pred results
-  where
-    results = (0, a, 1, 0) : (0, b, 0, 1) : zipWith step results (tail results)
-    step :: (Int, Int, Int, Int) -> (Int, Int, Int, Int) -> (Int, Int, Int, Int)
-    step (q0, r0, s0, t0) (q1, r1, s1, t1) = (q2, r2, s2, t2)
+    results = (a, 1, 0) : (b, 0, 1) : zipWith step results (tail results)
+    step :: Euclid -> Euclid -> Euclid
+    step (r0, s0, t0) (r1, s1, t1) = (r2, s2, t2)
       where
-        (q2, r2) = quotRem r0 r1 
-        s2       = s0 - (q2 * s1)
-        t2       = t0 - (q2 * t1)
-    pred :: (Int, Int, Int, Int) -> Bool
-    pred (_, r, _, _) = r /= 0
+        (q, r2) = quotRem r0 r1
+        s2      = s0 - (q * s1)
+        t2      = t0 - (q * t1)
+
+mmiLcm :: Integer -> Integer -> (Integer, Integer)
+mmiLcm n m = (s, (n * m) `div` r)
+  where
+    (r, s, _) = euclid n m
+
+mmi :: Integer -> Integer -> Integer
+mmi n m = mmi' `absMod` m
+  where
+    (mmi', _) = mmiLcm n m 
+
+absMod :: Integer -> Integer -> Integer
+absMod = (abs .) . mod
+
+combineTimes :: Integer -> (a -> a -> a) -> a -> a
+combineTimes n f x = foldl1 f $ map snd $ filter fst $ zip bin fs
+  where
+    fs  = iterate (join f) x
+    bin = dec2bin n
+
+dec2bin :: Integer -> [Bool]
+dec2bin 0 = []
+dec2bin n = odd n : dec2bin (n `div` 2)
