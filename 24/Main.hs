8b6cfb809ba1f8217ffc079ff1393a2b0cb6dab7
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2021-01-22 15:57:55 +0200

    Day 24 part 2

--- 24/Main.hs
+++ 24/Main.hs
@@ -1,55 +1,77 @@
 module Main where
 
-import Data.Functor ((<&>))
-import Data.List (intercalate)
-import Data.Maybe (maybe)
-import Data.Set (Set, empty, insert, member)
-import Data.Vector (Vector)
+import Data.Bits (popCount, setBit, testBit, zeroBits)
+import Data.Int (Int32)
+import Data.List (dropWhileEnd, foldl', intercalate)
+import Data.List.Split (chunksOf)
+import Data.Vector (Vector, (!), fromList)
 
-import Util ((!!?), (!!!), bool, filterBy, from2DList, positions, readWith, to2DList)
+import Util (bool, filterBy, readWith)
 
-type Position = (Int, Int)
-type Tiles = Vector (Vector Bool)
+bitsToNum :: [Int] -> Int32
+bitsToNum = foldl' setBit zeroBits
 
-readTiles :: [String] -> Tiles
-readTiles = from2DList . fmap (fmap (== '#'))
-
-showTiles :: Tiles -> String
-showTiles tiles = intercalate "\n" (fmap (bool '.' '#') <$> to2DList tiles) ++ "\n"
-
-adjacentPositions :: Position -> [Position]
-adjacentPositions (x, y) = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
-
-countBugs :: Tiles -> Position -> Int
-countBugs tiles position = sum $ maybe 0 (bool 0 1) . (tiles !!?) <$> adjacentPositions position
-
-nextTile :: Tiles -> Position -> Bool
-nextTile tiles position = case countBugs tiles position of
-  1 -> True
-  2 -> not $ tiles !!! position
-  _ -> False
-
-nextTiles :: Tiles -> Tiles
-nextTiles tiles = from2DList $ (nextTile tiles <$>) <$> positions 5 5
-
-biodiversity :: [Integer]
-biodiversity = take 25 $ iterate (* 2) 1
-
-calculateBiodiversity :: Tiles -> Integer
-calculateBiodiversity tiles = sum $ filterBy (concat $ to2DList tiles) biodiversity
-
-findDuplicate :: Tiles -> Tiles
-findDuplicate = step empty
+readGrid :: [String] -> Int32
+readGrid lines = bitsToNum $ filterBy setBits [0..24]
   where
-    step :: Set Integer -> Tiles -> Tiles
-    step seen tiles 
-        | bd `member` seen = tiles
-        | otherwise        = step (insert bd seen) (nextTiles tiles)
-      where
-        bd = calculateBiodiversity tiles
+    setBits = fmap (== '#') =<< lines
+
+showGrid :: Int32 -> String
+showGrid grid = intercalate "\n" (chunksOf 5 $ bool '.' '#' . testBit grid <$> [0..24]) ++ "\n"
+
+checkBits :: Vector [[Int]]
+checkBits = fromList [ [[7, 11], [1, 5], []]
+                     , [[7], [0, 2, 6], []]
+                     , [[7], [1, 3, 7], []]
+                     , [[7], [2, 4, 8], []]
+                     , [[7, 13], [3, 9], []]
+                     , [[11], [0, 6, 10], []]
+                     , [[], [1, 5, 7, 11], []]
+                     , [[], [2, 6, 8], [0, 1, 2, 3, 4]]
+                     , [[], [3, 7, 9, 13], []]
+                     , [[13], [4, 8, 14], []]
+                     , [[11], [5, 11, 15], []]
+                     , [[], [6, 10, 16], [0, 5, 10, 15, 20]]
+                     , [[], [], []]
+                     , [[], [8, 14, 18], [4, 9, 14, 19, 24]]
+                     , [[13], [9, 13, 19], []]
+                     , [[11], [10, 16, 20], []]
+                     , [[], [11, 15, 17, 21], []]
+                     , [[], [16, 18, 22], [20, 21, 22, 23, 24]]
+                     , [[], [13, 17, 19, 23], []]
+                     , [[13], [14, 18, 24], []]
+                     , [[11, 17], [15, 21], []]
+                     , [[17], [16, 20, 22], []]
+                     , [[17], [17, 21, 23], []]
+                     , [[17], [18, 22, 24], []]
+                     , [[13, 17], [19, 23], []]
+                     ]
+
+countBits :: Int32 -> [Int] -> Int
+countBits = (length .) . filter . testBit
+
+nextTile :: [Int32] -> Int -> Bool
+nextTile _ 12                 = False
+nextTile grids@[_, curr, _] i = case bugs of
+    1 -> True
+    2 -> not $ testBit curr i
+    _ -> False
+  where
+    bugs = sum $ zipWith countBits grids (checkBits ! i)
+
+nextGrid :: Int32 -> Int32 -> Int32 -> Int32
+nextGrid upper current lower = bitsToNum $ filter (nextTile [upper, current, lower]) [0..24]
+
+nextGrids :: [Int32] -> [Int32]
+nextGrids grids = dropWhileEnd (== 0) $ dropWhile (== 0) $ zipWith3 nextGrid grids' (tail grids') (tail (tail grids'))
+  where
+    grids' = 0 : 0 : grids ++ [0, 0]
+
+countBugs :: [Int32] -> Int
+countBugs = sum . map popCount
 
 main :: IO ()
-main = do tiles <- readWith readTiles
-          let duplicate = findDuplicate tiles
-          putStrLn $ showTiles duplicate
-          print $ calculateBiodiversity duplicate
+main = do grid <- readWith readGrid
+          let grids = iterate nextGrids [grid]
+              result = grids !! 200
+          print $ countBugs result
